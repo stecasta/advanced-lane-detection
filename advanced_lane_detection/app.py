175068729c55e83bcee9682635eb07dffbc3a096
stecasta <stefanocastagnetta@gmail.com>
2020-04-10 18:32:30 +0200

    Find lane region

--- advanced_lane_detection/app.py
+++ advanced_lane_detection/app.py
@@ -7,26 +7,47 @@ from advanced_lane_detection.utils import utils
 
 images = glob.glob('../camera_cal/calibration*.jpg') # images used for camera calibration
 
+# Calibrate camera.
 nx = 9 # number of inside corners in x
 ny = 6 # number of inside corners in y
-
-# Calibrate camera.
 imgpoints, objpoints = utils.calibrate_cam(images, nx, ny)
 
 # Read the image.
-image = mpimg.imread("../test_images/test3.jpg")
+image = mpimg.imread("../test_images/test2.jpg")
 
 # Undistort image.
 undistorted = utils.cal_undistort(image, objpoints, imgpoints)
 
 # Create thresholded binary image.
-segmented = utils.segment(undistorted)
+s_thresh=(170, 255) # saturation channel threshold
+sx_thresh=(20, 100) # sobelx threshold
+segmented = utils.segment(undistorted, s_thresh, sx_thresh)
+binary_img = np.zeros_like(segmented[:,:,0])
+binary_img[(segmented[:,:,1] > 0) | (segmented[:,:,2] > 0)] = 1
 
 # Warp image.
-warped_img = utils.warp(segmented)
+warped_img = utils.warp(binary_img)
 
-# Segment the two lanes
-lanes_img = utils.fit_polynomial(warped_img)
+# cv2.line(image, (255, 674), (1055, 674), (255, 0, 0), 4)
+# cv2.line(image, (1055, 674), (690, 450), (255, 0, 0), 4)
+# cv2.line(image, (590, 450), (690, 450), (255, 0, 0), 4)
+# cv2.line(image, (590, 450), (255, 674), (255, 0, 0), 4)
+#
+# cv2.line(warped_img, (300, 0), (950, 0), (255, 0, 0), 4)
+# cv2.line(warped_img, (300, warped_img.shape[0]), (950, warped_img.shape[0]), (255, 0, 0), 4)
+# cv2.line(warped_img, (300, warped_img.shape[0]), (300, 0), (255, 0, 0), 4)
+# cv2.line(warped_img, (950, warped_img.shape[0]), (950, 0), (255, 0, 0), 4)
+#
+# f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+# ax1.set_title('Source')
+# ax1.imshow(image)
+# ax2.set_title('Lanes')
+# ax2.imshow(warped_img)
+# plt.waitforbuttonpress()
+
+
+# Find lanes.
+lanes_img, ploty, left_fit_real, right_fit_real = utils.fit_polynomial(warped_img)
 
 # Unwarp image.
 unwarped_img = utils.unwarp(lanes_img)
@@ -34,6 +55,22 @@ unwarped_img = utils.unwarp(lanes_img)
 # Merge the two images.
 output_img = utils.weighted_img(image, unwarped_img, α=0.8, β=1., γ=0.)
 
+# Calculate the radius of curvature in meters for both lane lines.
+left_curverad, right_curverad = utils.measure_curvature(ploty, left_fit_real, right_fit_real)
+curverad = int((left_curverad + right_curverad) / 2)
+
+# Calculate position of the vehicle in the lane.
+offset = utils.measure_pos_in_lane(binary_img)
+if (offset > 0):
+    pos = "right"
+else:
+    pos = "left"
+
+# Write info on image
+text1 = "The radius of curvature is {} (m).".format(curverad)
+utils.write_on_image(output_img, text1, (80, 50))
+text2 = "Vehicle is %.2f (m) %s of center." % (np.absolute(offset), pos)
+utils.write_on_image(output_img, text2, (80, 120))
 
 # Plot.
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
--- advanced_lane_detection/utils/utils.py
+++ advanced_lane_detection/utils/utils.py
@@ -4,26 +4,6 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-# load the image, convert it to grayscale, and show it
-# image = cv2.imread("test_images/test1.jpg")
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# cv2.imshow("Image", image)
-# cv2.imshow("Gray", gray)
-# cv2.waitKey(0)
-# # construct a grayscale histogram
-# hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
-# # plot the histogram
-# plt.figure()
-# plt.title("Grayscale Histogram")
-# plt.xlabel("Bins")
-# plt.ylabel("# of Pixels")
-# plt.plot(hist)
-# plt.xlim([0, 256])
-# plt.show()
-# cv2.waitKey(0)
-
-# Camera calibration.
-# prepare object points
 
 def calibrate_cam(images, nx, ny):
 
@@ -82,7 +62,7 @@ def cal_undistort(img, objpoints, imgpoints):
 #
 # plt.waitforbuttonpress()
 
-def segment(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+def segment(img, s_thresh, sx_thresh):
     img = np.copy(img)
     # Convert to HLS color space and separate the V channel
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
@@ -100,6 +80,7 @@ def segment(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     # Threshold color channel
     s_binary = np.zeros_like(s_channel)
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+
     # Stack each channel
     color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
     return color_binary
@@ -107,19 +88,17 @@ def segment(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
 def warp(img):
     img_size = (img.shape[1], img.shape[0])
 
-    # Four source coords.
-    # TODO: consider tuning points better.
     src = np.float32(
-        [[265, 674],
-         [1039, 674],
-         [552, 478],
-         [727, 478]])
+        [[255, 674],
+         [1055, 674],
+         [590, 450],
+         [690, 450]])
 
     dst = np.float32(
-        [[400, 718],
-         [900, 718],
-         [400, 0],
-         [900, 0]])
+        [[300, img.shape[0]],
+         [950, img.shape[0]],
+         [300, 0],
+         [950, 0]])
 
     M = cv2.getPerspectiveTransform(src, dst)
 
@@ -130,19 +109,17 @@ def warp(img):
 def unwarp(img):
     img_size = (img.shape[1], img.shape[0])
 
-    # Four source coords.
-    # TODO: consider tuning points better.
     src = np.float32(
-        [[265, 674],
-         [1039, 674],
-         [552, 478],
-         [727, 478]])
+        [[255, 674],
+         [1055, 674],
+         [590, 450],
+         [690, 450]])
 
     dst = np.float32(
-        [[400, 718],
-         [900, 718],
-         [400, 0],
-         [900, 0]])
+        [[300, img.shape[0]],
+         [950, img.shape[0]],
+         [300, 0],
+         [950, 0]])
 
     Minv = cv2.getPerspectiveTransform(dst, src)
 
@@ -152,15 +129,9 @@ def unwarp(img):
 
 def find_lane_pixels(binary_warped):
     # Take a histogram of the bottom half of the image
-    # histogram = np.sum(binary_warped[binary_warped.shape[0] // 2:, :], axis=0)
-    # TODO: consider feeding directly a grayscale img
-    gray_warped = cv2.cvtColor(binary_warped, cv2.COLOR_BGR2GRAY)
-    histogram = np.sum(gray_warped, axis=0)
-    # plt.plot(histogram)
-    # plt.waitforbuttonpress()
+    histogram = np.sum(binary_warped[binary_warped.shape[0] // 2:, :], axis=0)
     # Create an output image to draw on and visualize the result
-    # out_img = np.dstack((binary_warped, binary_warped, binary_warped))
-    out_img = binary_warped
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))
     # Find the peak of the left and right halves of the histogram
     # These will be the starting point for the left and right lines
     midpoint = np.int(histogram.shape[0] // 2)
@@ -191,19 +162,22 @@ def find_lane_pixels(binary_warped):
 
     # Step through the windows one by one
     for window in range(nwindows):
-        # Identify window boundaries in x and y (and right and left).
+        # Identify window boundaries in x and y (and right and left)
         win_y_low = binary_warped.shape[0] - (window + 1) * window_height
         win_y_high = binary_warped.shape[0] - window * window_height
-        # Find the four below boundaries of the window.
-        win_xleft_low = leftx_current - margin
-        win_xleft_high = leftx_current + margin
-        win_xright_low = rightx_current - margin
-        win_xright_high = rightx_current + margin
+        ### TO-DO: Find the four below boundaries of the window ###
+        win_xleft_low = leftx_current - margin  # Update this
+        win_xleft_high = leftx_current + margin  # Update this
+        win_xright_low = rightx_current - margin  # Update this
+        win_xright_high = rightx_current + margin  # Update this
 
         # Draw the windows on the visualization image
-        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
-        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
-        ### Identify the nonzero pixels in x and y within the window ###
+        # cv2.rectangle(out_img, (win_xleft_low, win_y_low),
+        #               (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        # cv2.rectangle(out_img, (win_xright_low, win_y_low),
+        #               (win_xright_high, win_y_high), (0, 255, 0), 2)
+
+        ### TO-DO: Identify the nonzero pixels in x and y within the window ###
         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
                           (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
         good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
@@ -213,7 +187,7 @@ def find_lane_pixels(binary_warped):
         left_lane_inds.append(good_left_inds)
         right_lane_inds.append(good_right_inds)
 
-        ### If you found > minpix pixels, recenter next window ###
+        ### TO-DO: If you found > minpix pixels, recenter next window ###
         ### (`right` or `leftx_current`) on their mean position ###
         if (sum(good_left_inds > minpix)):
             leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
@@ -240,10 +214,9 @@ def fit_polynomial(binary_warped):
     # Find our lane pixels first
     leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
 
-    ###Fit a second order polynomial to each using `np.polyfit` ###
+    # Fit a second order polynomial to each using `np.polyfit`
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
-
     # Generate x and y values for plotting
     ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
     try:
@@ -255,16 +228,31 @@ def fit_polynomial(binary_warped):
         left_fitx = 1 * ploty ** 2 + 1 * ploty
         right_fitx = 1 * ploty ** 2 + 1 * ploty
 
+    ym_per_pix = 30 / 720  # meters per pixel in y dimension
+    xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+
+    left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+
     ## Visualization ##
     # Colors in the left and right lane regions
     out_img[lefty, leftx] = [255, 0, 0]
     out_img[righty, rightx] = [0, 0, 255]
 
-    # Plots the left and right polynomials on the lane lines
-    plt.plot(left_fitx, ploty, color='yellow')
-    plt.plot(right_fitx, ploty, color='yellow')
+    # Prepare points to be used to defined the lane region.
+    draw_points_l = (np.asarray([left_fitx, ploty]).T).astype(np.int32)  # needs to be int32 and transposed
+    draw_points_r = (np.asarray([right_fitx, ploty]).T).astype(np.int32)  # needs to be int32 and transposed
+    draw_points_r = np.flipud(draw_points_r)
+    points = np.concatenate((draw_points_l, draw_points_r))
 
-    return out_img
+    # Draw the left and right polynomials on the lane lines.
+    # cv2.polylines(out_img, [draw_points_l], False, (255, 255, 0), 4)  # args: image, points, closed, color
+    # cv2.polylines(out_img, [draw_points_r], False, (255, 255, 0), 4)  # args: image, points, closed, color
+
+    # Fill lane region in green.
+    cv2.fillPoly(out_img, [points], (0,255,0))
+
+    return out_img, ploty, left_fit_cr, right_fit_cr
 
 def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
     """
@@ -279,3 +267,61 @@ def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
     NOTE: initial_img and img must be the same shape!
     """
     return cv2.addWeighted(initial_img, α, img, β, γ)
+
+def measure_curvature(ploty, left_fit_cr, right_fit_cr):
+    '''
+    Calculates the curvature of polynomial functions in meters.
+    '''
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30 / 720  # meters per pixel in y dimension
+    xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+
+    # Define y-value where we want radius of curvature
+    # We'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = np.max(ploty)
+
+    # Calculation of R_curve (radius of curvature).
+    left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+        2 * left_fit_cr[0])
+    right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+        2 * right_fit_cr[0])
+
+    return left_curverad, right_curverad
+
+def measure_pos_in_lane(img):
+    # Lane is 3.7 meters wide.
+    # Camera is mounted at the center of the vehicle.
+    # Find center of the lane and compute offset with center of the camera.
+
+    xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+
+    # Take a histogram of the bottom of the image
+    histogram = np.sum(img[670:690, :], axis=0)
+
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0] // 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    # print(leftx_base, rightx_base)
+    pos  = (leftx_base + rightx_base) / 2
+    # print(pos - img.shape[1] / 2)
+    offset = ((img.shape[1] / 2) - pos) * xm_per_pix
+    # print(offset)
+    return offset
+
+def write_on_image(img, text, bottomLeftCornerOfText):
+    # Write some Text
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 2
+    fontColor = (255, 255, 255)
+    lineType = 2
+
+    cv2.putText(img, text,
+                bottomLeftCornerOfText,
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+    return
